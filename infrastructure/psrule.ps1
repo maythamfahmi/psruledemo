1d1ece8af7aa5141778408c054bc8dec94d815a0
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2025-02-27 20:41:13 +0100

    Update psrule.ps1

--- infrastructure/psrule.ps1
+++ infrastructure/psrule.ps1
@@ -1,1 +1,5 @@
+Install-Module -Name PSRule
+Install-Module -Name PSRule.Rules.Azure
+Install-Module -Name PSRule.Rules.CAF
+
 Assert-PSRule -Path .\tests\psrule\.ps-rule\ -InputPath .\ -Option .\tests\psrule\ps-rule.yaml
